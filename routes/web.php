775e3235c90241aa5dacd57dccc3e2eccffc2606
AlanGabHahn <alan_hahn1995@outlook.com>
2023-04-17 22:50:38 -0300

    primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\SubcategorieController;
+use App\Http\Controllers\SubcategorieController; 
 
 /*
 |--------------------------------------------------------------------------
